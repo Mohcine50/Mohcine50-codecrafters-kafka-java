0ffba5db47a3c8cbec2df560cfe68a55fe45dbd8
shegami <mohcinela1998@gmail.com>
2025-02-04 15:47:14 +0100

    stage 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,6 +17,14 @@ public class Main {
        serverSocket.setReuseAddress(true);
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
+
+       OutputStream out = clientSocket.getOutputStream();
+
+
+       out.write(new byte[]{0, 0, 0, 0});
+       out.write(new byte[]{0, 0, 0, 7});
+       out.flush();
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {

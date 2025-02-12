f5b3b9c76ded0f772424ca1c9681c533365551b1
Mohcine Lahnin <mohcinela1998@gmail.com>
2025-02-12 18:24:30 +0100

    test

--- src/main/java/kafka/request/RequestHandler.java
+++ src/main/java/kafka/request/RequestHandler.java
@@ -7,7 +7,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.HexFormat;
 import java.util.Map;
 import java.util.Base64.Decoder;
 
@@ -49,11 +52,13 @@ import static constants.Constants.UNKNOWN_TOPIC_OR_PARTITION;
 public class RequestHandler {
 
     private Socket clientSocket;
+    private Map<String, byte[]> parseStreamMap = new HashMap<String, byte[]>();
 
     public RequestHandler(Socket socket) {
         this.clientSocket = socket;
     }
 
+    @SuppressWarnings("static-access")
     public void handleRequest() {
 
         try {
@@ -62,18 +67,12 @@ public class RequestHandler {
                 OutputStream out = clientSocket.getOutputStream();
                 InputStream in = clientSocket.getInputStream();
                 // We parse the input from the Input Stream
-                Map<String, Object> parseStreamMap = parseStream(in);
-                ByteBuffer.allocate(2);
-                short api_version = ByteBuffer.wrap((byte[]) parseStreamMap.get(API_VERSION)).getShort();
-                short api_key = ByteBuffer.wrap((byte[]) parseStreamMap.get(API_KEY)).getShort();
-                System.out.println(api_key + " " + api_version);
-                byte topicNameLength = (byte) parseStreamMap.get(TOPIC_NAME_LENGTH);
-                String topicName = (String) parseStreamMap.get(TOPIC_NAME);
+                parseStream(in);
 
-                ByteArrayOutputStream baos = get_response((byte[]) parseStreamMap.get(CORRELATION_ID), api_version,
-                        api_key, topicNameLength, topicName);
+                ByteArrayOutputStream baos = get_response();
 
                 int message_size = baos.size();
+
                 byte[] response = baos.toByteArray();
 
                 byte[] size_byte = ByteBuffer.allocate(4).putInt(message_size).array();
@@ -89,64 +88,57 @@ public class RequestHandler {
     }
 
     @SuppressWarnings("static-access")
-    private Map<String, Object> parseStream(InputStream in) throws IOException {
+    private void parseStream(InputStream in) throws IOException {
 
         DataInputStream dataInputStream = new DataInputStream(in);
-
-        Map<String, Object> map = new HashMap<String, Object>();
 
         byte[] message_size = dataInputStream.readNBytes(4);
         byte[] request_api_key = dataInputStream.readNBytes(2);
         byte[] request_api_version = dataInputStream.readNBytes(2);
         byte[] correlation_id = dataInputStream.readNBytes(4);
 
-        byte[] remainingBytes = null;
         short api_key = ByteBuffer.allocate(2).wrap(request_api_key).getShort();
 
-        map.put(MESSAGE_SIZE, message_size);
-        map.put(API_KEY, request_api_key);
-        map.put(API_VERSION, request_api_version);
-        map.put(CORRELATION_ID, correlation_id);
+        parseStreamMap.put(MESSAGE_SIZE, message_size);
+        parseStreamMap.put(API_KEY, request_api_key);
+        parseStreamMap.put(API_VERSION, request_api_version);
+        parseStreamMap.put(CORRELATION_ID, correlation_id);
 
-        System.out.println(api_key);
         // Check if the request api version is for Desccribe topic partitions
         if (api_key == DESCRIBETOPICPARTITIONS) {
-            System.out.println("inside the id");
-            Map<String, Object> desMap = parseDescriptionTopicPartitionMessage(dataInputStream);
-            System.out.println("inside the id");
-            map.putAll(desMap);
+            short length = dataInputStream.readShort();
+            byte[] content = dataInputStream.readNBytes(length);
+
+            dataInputStream.readNBytes(1); // tug buffer
+
+            byte[] topicArrayLength = dataInputStream.readNBytes(1);
+            byte[] topicNameLength = dataInputStream.readNBytes(1);
+            byte[] topicName = dataInputStream.readNBytes(topicNameLength[0] - 1);
+
+            dataInputStream.readNBytes(1); // tug buffer
+
+            byte[] partitions = dataInputStream.readNBytes(4);
+
+            byte[] cursor = dataInputStream.readNBytes(1);
+            dataInputStream.readNBytes(1); // tug buffer
+
+            System.out.println("heressssssssssss 1");
+            parseStreamMap.put(CONTENT, content);
+            System.out.println("heressssssssssss 2");
+            parseStreamMap.put(TOPIC_ARRAY_LENGTH, topicArrayLength);
+            System.out.println("heressssssssssss 3");
+            parseStreamMap.put(TOPIC_NAME_LENGTH, topicNameLength);
+            System.out.println("heressssssssssss 4");
+            parseStreamMap.put(TOPIC_NAME, topicName);
+            System.out.println("heressssssssssss 5");
+            parseStreamMap.put(PARTITIONS, partitions);
+            System.out.println("heressssssssssss 6");
+            parseStreamMap.put(CURSOR, cursor);
+
+            System.out.println("heressssssssssss");
         }
-
-        remainingBytes = new byte[dataInputStream.available()];
+        byte[] remainingBytes = new byte[dataInputStream.available()];
         dataInputStream.readFully(remainingBytes);
-
-        return map;
-    }
-
-    Map<String, Object> parseDescriptionTopicPartitionMessage(DataInputStream dataInputStream) throws IOException {
-
-        Map<String, Object> desMap = new HashMap<String, Object>();
-
-        short length = dataInputStream.readShort();
-        byte[] content = dataInputStream.readNBytes(length);
-        byte tagBuffer = dataInputStream.readByte();
-        short topicArrayLength = dataInputStream.readShort();
-        byte topicNameLength = dataInputStream.readByte();
-        String topicName = dataInputStream.readUTF();
-        byte tagBuffer_1 = dataInputStream.readByte();
-        byte[] partitions = dataInputStream.readNBytes(4);
-        byte cursor = dataInputStream.readByte();
-        byte tagBuffer_2 = dataInputStream.readByte();
-
-        desMap.put(LENGTH, length);
-        desMap.put(CONTENT, content);
-        desMap.put(TOPIC_ARRAY_LENGTH, topicArrayLength);
-        desMap.put(TOPIC_NAME_LENGTH, topicNameLength);
-        desMap.put(TOPIC_NAME, topicName);
-        desMap.put(PARTITIONS, partitions);
-        desMap.put(CURSOR, cursor);
-
-        return desMap;
     }
 
     /*
@@ -154,26 +146,32 @@ public class RequestHandler {
      * error_code [api_keys] throttle_time_ms TAG_BUFFER
      * Response for the DescribeTopicPartitions
      */
-    private ByteArrayOutputStream get_response(byte[] cId, short api_version, short api_key, byte topicNameLength,
-            String topicName) {
+    private ByteArrayOutputStream get_response() {
+
+        short apiVersion = ByteBuffer.allocate(2).wrap((byte[]) parseStreamMap.get(API_VERSION)).getShort();
+
+        short apiKey = ByteBuffer.allocate(2).wrap((byte[]) parseStreamMap.get(API_KEY)).getShort();
+
+        byte[] correlationId = parseStreamMap.get(CORRELATION_ID);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
         try {
+            baos.write(correlationId);
 
-            if (api_version < 0 || api_version > 4) {
-                switch (api_key) {
+            if (apiVersion < 0 || apiVersion > 4) {
+                switch (apiKey) {
                     case APIVERSIONS:
-                        sendApiVersions(baos, cId);
+                        sendApiVersions(baos);
                         break;
                     case DESCRIBETOPICPARTITIONS:
-                        sendDescribeTopicPartitionsResponse(baos, cId, topicNameLength, topicName);
+                        sendDescribeTopicPartitionsResponse(baos);
                         break;
                     default:
                         throw new Error("API KEY NOT SUPPORTED");
                 }
+
             } else {
-                baos.write(cId);
                 baos.write(new byte[] { 0, 35 });
             }
 
@@ -185,36 +183,40 @@ public class RequestHandler {
 
     }
 
-    void sendDescribeTopicPartitionsResponse(ByteArrayOutputStream baos, byte[] cId, byte topicNameLength,
-            String topicName)
+    void sendDescribeTopicPartitionsResponse(ByteArrayOutputStream baos)
             throws IOException {
 
-        baos.write(cId);
-        baos.write(new byte[] { 0, 0 });
-        baos.write(0);
-        baos.write(new byte[] { 0, 0, 0, 0 });
-        baos.write(2);
-        baos.write(new byte[] { 0, UNKNOWN_TOPIC_OR_PARTITION });
-        baos.write(new byte[] { topicNameLength });
-        baos.write(new String(topicName));
+        byte[] topicNameLength = parseStreamMap.get(TOPIC_NAME_LENGTH);
+        byte[] topicName = parseStreamMap.get(TOPIC_NAME);
+        byte[] arrayLength = parseStreamMap.get(TOPIC_ARRAY_LENGTH);
+
+        baos.write(0); // tag buffer
+        baos.write(new byte[] { 0, 0, 0, 0 }); // throtels time
+        baos.write(arrayLength[0]); // tag buffer : Array length
+        baos.write(new byte[] { 0, 3 });
+        baos.write(topicNameLength[0]);
+        baos.write(topicName);
         baos.write(new byte[16]);
         baos.write(0);
-        baos.write(1);
-        baos.write(new byte[] { 0, 0, (byte) 0x0d, (byte) 0xf8 });
+        baos.write(2);
+        baos.write(new byte[] { 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0 });
         baos.write(0);
-        baos.write(new byte[] { (byte) 0xff });
+        baos.write(0xFF);
         baos.write(0);
 
     }
 
-    void sendApiVersions(ByteArrayOutputStream baos, byte[] cId) throws IOException {
+    void sendApiVersions(ByteArrayOutputStream baos) throws IOException {
 
-        baos.write(cId);
         baos.write(new byte[] { 0, 0 });
-        baos.write(2);
+        baos.write(3);
         baos.write(new byte[] { 0, 18 });
         baos.write(new byte[] { 0, 0 });
         baos.write(new byte[] { 0, 4 });
+        baos.write(0);
+        baos.write(new byte[] { 0, 75 });
+        baos.write(new byte[] { 0, 0 });
+        baos.write(new byte[] { 0, 0 });
         baos.write(0);
         baos.write(new byte[] { 0, 0, 0, 0 });
         baos.write(0);
